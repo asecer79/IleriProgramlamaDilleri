4cb3bab31fab1a95a3959bcd716c3a430240a1d8
Aydın SEÇER <aydin_secer@hotmail.com>
2022-03-24 20:02:07 +0300

    e

---
+++ Week04_Inheritance/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Week04_Inheritance
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+
+
+
+    class A
+    {
+        public A()
+        {
+            
+        }
+
+        public int fieldA;
+
+        protected void MethodA()
+        {
+
+        }
+    }
+
+    class B : A
+    {
+        //public int fieldA;
+        public B()
+        {
+            MethodA();
+
+            this.fieldA = 5;
+            
+            base.fieldA=5;
+            
+        }
+
+        protected int fieldB;
+
+        public void MethodB()
+        {
+
+        }
+    }
+
+    class C : B
+    {
+        public int fieldC;
+        public C()
+        {
+            MethodA();
+            MethodB();
+            MethodC();
+
+            fieldA = 7;
+            fieldB = 6;
+            fieldC = 7;
+        }
+
+        public void MethodC()
+        {
+
+        }
+    }
+
+}
+
